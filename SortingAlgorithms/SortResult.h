827fc67edb541b8012b33a0fa5db1075cffb0da1
ubmervy <mervy@mail.ru>
2015-03-11 20:48:26 +0300

    SortResult separated

---
+++ SortingAlgorithms/SortResult.h
@@ -0,0 +1,25 @@
+//SortResult.h
+#ifndef SortResult_h
+#define SortResult_h
+#include <string>
+
+class SortResult
+{
+public:
+	//constructor
+	SortResult(std::string st, double dur = 0.0, int cm = 0, int mv = 0);
+
+	//copy constructor
+	SortResult(const SortResult& other_sortresult);
+
+	//destructor
+	~SortResult();
+
+	void SetSortType(std::string& s);
+
+	double duration; // sorting time
+	std::string sorttype; //sorting algorithm name
+	int cmp; // number of compares
+	int moves; // number of element's moves (exchanges)
+};
+#endif // !SortResult_h
